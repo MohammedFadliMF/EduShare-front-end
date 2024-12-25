7599fa47bd743851a6ddfa2f8ccdbe719e30c7b3
MohammedFadliMF <fadlimohamed155@gmail.com>
2024-12-25 22:59:48 +0000

    feat: add archived-courses component to project

---
+++ src/app/components/archived-courses/archived-courses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archived-courses',
+  standalone: true,
+  imports: [],
+  templateUrl: './archived-courses.component.html',
+  styleUrl: './archived-courses.component.css'
+})
+export class ArchivedCoursesComponent {
+
+}
