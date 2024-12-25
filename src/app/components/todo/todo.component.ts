82b50765c275472ad3f65a39a8e2d54a99bb2b7e
MohammedFadliMF <fadlimohamed155@gmail.com>
2024-12-25 23:01:11 +0000

    feat: add todo component to project

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+// import { MatTabsModule } from '@angular/material/tabs';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent {
+
+}
