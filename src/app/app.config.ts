3ae01223a6b689e644a6d3a678a3e61eed8cd69b
MohammedFadliMF <fadlimohamed155@gmail.com>
2024-12-25 22:41:49 +0000

    refactor: add CommonModule and BrowserAnimationsModule globally for standalone components

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(CommonModule, BrowserAnimationsModule),
+  ],
 };
