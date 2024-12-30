810ca4c35994913124ba99af819d07cef23a5666
MohammedFadliMF <fadlimohamed155@gmail.com>
2024-12-30 15:21:00 +0000

    feat(toolbar): improve toolbar component layout and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { CourseGridComponent } from './components/course-grid/course-grid.component';
-import { CourseBannerComponent } from './components/course-banner/course-banner.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CustomSidenavComponent } from './components/custom-sidenav/custom-sidenav.component';
 
 @Component({
   selector: 'app-root',
@@ -15,17 +13,16 @@ import { CourseBannerComponent } from './components/course-banner/course-banner.
   imports: [
     RouterOutlet,
     CommonModule,
+    MatToolbarModule,
     MatButtonModule,
-    MatDividerModule,
     MatIconModule,
-    HeaderComponent,
-    SidebarComponent,
-    CourseGridComponent,
-    CourseBannerComponent,
+    MatSidenavModule,
+    CustomSidenavComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'EduShare-front-end';
+  collapsed = signal(false);
+  sidenavWidth = computed(() => (this.collapsed() ? '65px' : '250px'));
 }
